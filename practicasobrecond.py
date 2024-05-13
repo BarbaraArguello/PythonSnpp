559753b2cc3eb16d5c1c6a57c444897fc5d7ea67
BarbaraArguello <barbaraarg22@gmail.com>
2024-05-13 16:10:43 -0400

    P.1 Práctica sobre condicionales y bucles
    practica 1 sobre condicionales y bucles

---
+++ practicasobrecond.py
@@ -0,0 +1,12 @@
+'''leer tres notas por teclado y cragarlos en una lista.
+Calcular el promedio y si está aprobado o repobrado.
+Si el promedio >1.7 = aprobado sino reprobado'''
+
+notas=[]
+#leer datos
+for i in range(3):
+  nota  = input(f"Ingrese la nota {x]: ")
+  notas.append(nota)
+#Calcular el promedio y si etsá aprobado o reprobado
+#def promedio(nota):
+  #x =
